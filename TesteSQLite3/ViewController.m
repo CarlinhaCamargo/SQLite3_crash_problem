82f08c13182af2c2c078148908392e2e11c0d23d
Robert Ryan <robert.ryan@mindspring.com>
2020-06-03 04:49:17 -0700

    Add SQLCipher via CocoaPods
    * Added SQLCipher commands to confirm that encryption works properly
    * Moved "open db" code into method that also sets the key
    
    Note,
    
    * If you already have unencrypted db in Documents path, you might get cryptic "26 - file is not a database" message.
    * Make sure to open `.xcworkspace` not `.xcodeproj`

--- TesteSQLite3/ViewController.m
+++ TesteSQLite3/ViewController.m
@@ -20,8 +20,16 @@ static NSString* systemSqliteDBFileName = @"testdb.sqlite";
 - (void)viewDidLoad {
     [super viewDidLoad];
     NSURL* documentsDir = [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
-           
+
     self.dbFilePath = [documentsDir.path stringByAppendingPathComponent:systemSqliteDBFileName];
+
+    if (![self createDB]) {
+        NSLog(@"Error - failed to create/open a database.");
+        return;
+    }
+
+    [self testSqlCipher];
+
     // Do any additional setup after loading the view.
     NSArray *array = [[NSArray alloc] initWithArray: [self columnMethodsTest]];
     for (NSString *string in array) {
@@ -37,9 +45,10 @@ static NSString* systemSqliteDBFileName = @"testdb.sqlite";
     if ([self dbExists]) {
         return YES;
     }
-    
+
     sqlite3* dbConnection;
-    if (SQLITE_OK == sqlite3_open([self.dbFilePath UTF8String], &dbConnection)) {
+
+    if ((dbConnection = [self openDatabase])) {
         char* errorMsg = NULL;
         const char* sqlCreateStatement = "create table if not exists \
         empInfo (id integer primary key, firstname text, lastname text, address text);";
@@ -54,30 +63,21 @@ static NSString* systemSqliteDBFileName = @"testdb.sqlite";
             NSLog(@"Error executing sqlite3 query:\"%@\" - error message:%@",
                   [NSString stringWithCString:sqlCreateStatement encoding:NSASCIIStringEncoding],
                   errorMsgFromExec);
+            sqlite3_close(dbConnection);
             return NO;
         }
         sqlite3_close(dbConnection);
         return YES;
     }
     
-    sqlite3_close(dbConnection); // you need to close this even if open failed
     return NO;
 }
 
 - (NSArray*)columnMethodsTest {
     NSMutableArray *columnInfo = [[NSMutableArray alloc] init];
 
-    if (![self createDB]) {
-        NSLog(@"Error - failed to create/open a database.");
-        return columnInfo;
-    }
-
     sqlite3* dbConnection;
-    if (SQLITE_OK != sqlite3_open([self.dbFilePath UTF8String], &dbConnection)) {
-        // Fail to open the database.
-        NSLog(@"Error - failed to open the database. %s", sqlite3_errmsg(dbConnection) ?: "Unknown error");
-
-        sqlite3_close(dbConnection);
+    if (!(dbConnection = [self openDatabase])) {
         return columnInfo;
     }
 
@@ -131,6 +131,69 @@ static NSString* systemSqliteDBFileName = @"testdb.sqlite";
     sqlite3_close(dbConnection);
 
     return columnInfo;
+}
+
+- (BOOL)setKeyForDatabase:(sqlite3 *)db {
+    int rc;
+    const char* key = [@"BIGSecret" UTF8String];
+    if ((rc = sqlite3_key(db, key, (int)strlen(key))) != SQLITE_OK) {
+        NSLog(@"sqlite3_key error: %d %s", rc, sqlite3_errmsg(db));
+    }
+    return rc == SQLITE_OK;
+}
+
+- (sqlite3 *)openDatabase {
+    sqlite3 *db;
+    int rc;
+
+    if ((rc = sqlite3_open([self.dbFilePath UTF8String], &db)) != SQLITE_OK) {
+        NSLog(@"sqlite3_open error: %d %s", rc, sqlite3_errmsg(db));
+        sqlite3_close(db);
+        return NULL;
+    }
+
+    if (![self setKeyForDatabase:db]) {
+        sqlite3_close(db);
+        return NULL;
+    }
+
+    return db;
+}
+
+- (void)testSqlCipher {
+    sqlite3 *db;
+    sqlite3_stmt *stmt;
+    int rc;
+    bool sqlcipher_valid = NO;
+
+    if (!(db = [self openDatabase])) {
+        return;
+    }
+
+    if ((rc = sqlite3_exec(db, (const char*) "SELECT count(*) FROM sqlite_master;", NULL, NULL, NULL)) != SQLITE_OK) {
+        NSLog(@"sqlite3_exec error: %d %s", rc, sqlite3_errmsg(db));
+    }
+
+    if ((rc = sqlite3_prepare_v2(db, "PRAGMA cipher_version;", -1, &stmt, NULL)) != SQLITE_OK) {
+        NSLog(@"sqlite3_prepare_v2 error: %d %s", rc, sqlite3_errmsg(db));
+        sqlite3_close(db);
+        return;
+    }
+
+    if ((rc = sqlite3_step(stmt) != SQLITE_ROW)) {
+        NSLog(@"sqlite3_step error: %d %s", rc, sqlite3_errmsg(db));
+    }
+
+    const unsigned char *ver = sqlite3_column_text(stmt, 0);
+    if (ver != NULL) {
+        sqlcipher_valid = YES;
+        NSLog(@"cipher_version = %s", ver);
+        // password is correct (or database initialize), and verified to be using sqlcipher
+    }
+
+    sqlite3_finalize(stmt);
+
+    sqlite3_close(db);
 }
 
 @end
